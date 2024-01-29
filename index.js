d24ec2b5d69c129a989065c3ef078a3829c7882e
unknown <kov1606@gmail.com>
2024-01-29 23:24:23 +0300

    Добавление постов на страницу

---
+++ index.js
@@ -0,0 +1,43 @@
+const posts = document.querySelector('.posts');
+const btn = document.querySelector('.post__btn');
+const title = document.querySelector('#post__title');
+const body = document.querySelector('#post__body');
+const form = document.querySelector('.form');
+
+function addPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: title.value,
+            body: body.value,
+            userId: 1
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then((json) => {
+        const divPost = document.createElement('div');
+        divPost.classList.add('post');
+        posts.appendChild(divPost)
+
+        const pTitle = document.createElement('p');
+        pTitle.classList.add('post__title');
+        pTitle.textContent = json.title;
+        divPost.appendChild(pTitle);
+
+        const pBody = document.createElement('p');
+        pBody.classList.add('post__body');
+        pBody.textContent = json.body;
+        divPost.appendChild(pBody);
+
+    })
+    .catch(err => console.log('Ошибка: ' + err));
+}
+
+btn.addEventListener('click',  function(event) {
+    event.preventDefault();
+    addPost();
+    form.reset();
+});
